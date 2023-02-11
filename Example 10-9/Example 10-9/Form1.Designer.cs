c3975d334db1b11cf5ec1dca0240166723ef0822
MahanRajabi80 <105061018+MahanRajabi80@users.noreply.github.com>
2023-02-11 17:59:41 +0330

    Add files via upload

---
+++ Example 10-9/Example 10-9/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Example_10_9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtInputDigit = new System.Windows.Forms.TextBox();
+            this.btnConvertFarsi = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Digit (Maxlength=12):";
+            // 
+            // txtInputDigit
+            // 
+            this.txtInputDigit.Location = new System.Drawing.Point(154, 37);
+            this.txtInputDigit.MaxLength = 12;
+            this.txtInputDigit.Name = "txtInputDigit";
+            this.txtInputDigit.Size = new System.Drawing.Size(301, 20);
+            this.txtInputDigit.TabIndex = 0;
+            // 
+            // btnConvertFarsi
+            // 
+            this.btnConvertFarsi.Location = new System.Drawing.Point(237, 80);
+            this.btnConvertFarsi.Name = "btnConvertFarsi";
+            this.btnConvertFarsi.Size = new System.Drawing.Size(75, 39);
+            this.btnConvertFarsi.TabIndex = 1;
+            this.btnConvertFarsi.Text = "Show Farsi";
+            this.btnConvertFarsi.UseVisualStyleBackColor = true;
+            this.btnConvertFarsi.Click += new System.EventHandler(this.btnConvertFarsi_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblResult.Location = new System.Drawing.Point(12, 136);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lblResult.Size = new System.Drawing.Size(459, 27);
+            this.lblResult.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 172);
+            this.Controls.Add(this.btnConvertFarsi);
+            this.Controls.Add(this.txtInputDigit);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Example 10-9 (Method9 - Convert Digit To Farsi)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtInputDigit;
+        private System.Windows.Forms.Button btnConvertFarsi;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ Example 10-9/Example 10-9/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Example_10_9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //-----------------------------------------------------------------------
+        private string Equivalent3(string s)
+        {
+            System.Collections.Hashtable arr = new System.Collections.Hashtable();
+            //sefr aval return صفر  vali baghi sefrha return ""
+            arr.Add(0, ""); arr.Add(1, "یک"); arr.Add(2, "دو"); arr.Add(3, "سه");
+            arr.Add(4, "چهار"); arr.Add(5, "پنج"); arr.Add(6, "شش"); arr.Add(7, "هفت");
+            arr.Add(8, "هشت"); arr.Add(9, "نه"); arr.Add(10, "ده"); arr.Add(11, "یازده");
+            arr.Add(12, "دوازده"); arr.Add(13, "سیزده"); arr.Add(14, "چهارده"); arr.Add(15, "پانزده");
+            arr.Add(16, "شانزده"); arr.Add(17, "هفده"); arr.Add(18, "هجده"); arr.Add(19, "نوزده");
+            arr.Add(20, "بیست"); arr.Add(30, "سی"); arr.Add(40, "چهل"); arr.Add(50, "پنجاه");
+            arr.Add(60, "شصت"); arr.Add(70, "هفتاد"); arr.Add(80, "هشتاد"); arr.Add(90, "نود");
+            arr.Add(100, "صد"); arr.Add(200, "دویست"); arr.Add(300, "سیصد");arr.Add(400, "چهارصد");
+            arr.Add(500, "پانصد"); arr.Add(600, "ششصد"); arr.Add(700, "هفتصد");
+            arr.Add(800, "هشتصد"); arr.Add(900, "نهصد");
+            ///////////////
+            object o = int.Parse(s);//chon adad bala az no int hastand // agar long bezarim khata darad
+            if (!arr.ContainsKey(o))
+            {
+                int a = int.Parse(s);
+                s = "";
+                int i = 100;
+                while (a != 0)
+                {
+                    s = s +  arr[a / i * i].ToString()+" ";
+                    a = a - a / i * i;
+                    i = i / 10;
+                }
+                return s;
+            }
+            else
+                return arr[o].ToString();
+        }
+        //-----------------------------------------------------------------------
+        private string ConvertToFarsi(string digit)
+        {
+            if (digit == "0" || digit == "")
+            {
+                return "صفر";
+            }
+            digit = Math.Abs(long.Parse(digit)).ToString();
+            string result = "";
+            int i = 0;
+            while (digit != "")
+            {
+                string s;
+                if (digit.Length >= 3)
+                    s = digit.Substring(digit.Length - 3, 3);
+                else
+                    s = digit;
+                switch(i)
+                {
+                    case 0: result = Equivalent3(s) + " " + result; break;
+                    case 1: result = Equivalent3(s) == "" ? result : Equivalent3(s) + " هزار " + result; break;
+                    case 2: result = Equivalent3(s) == "" ? result : Equivalent3(s) + " میلیون " + result; break;
+                    case 3: result = Equivalent3(s) == "" ? result : Equivalent3(s) + " میلیارد " + result; break;
+                }
+                if (digit.Length >= 3)
+                    digit = digit.Remove(digit.Length - 3, 3);
+                else
+                    digit = "";
+                i++;
+            }
+            return result;
+        }
+        //-----------------------------------------------------------------------
+        private void btnConvertFarsi_Click(object sender, EventArgs e)
+        {
+            lblResult.Text = ConvertToFarsi(txtInputDigit.Text);
+        }
+    }
+}
